c384b434b27d1b194c37b5c2ec447233efcd7660
KauanSarzi <kauansarzi57@gmail.com>
2025-04-05 21:28:18 +0000

    exercicio1

---
+++ Excessao/src/App.java
@@ -0,0 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            String [] vet = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vet[position]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("POSIÇÃO INVALIDA");   
+        }
+        catch(InputMismatchException e){
+            System.out.println("ERRO NA ENTRADA DE DADOS");
+        }
+        
+        System.out.println("FIM DO PROGRAMA!");
+        sc.close();
+
+    }
+}
